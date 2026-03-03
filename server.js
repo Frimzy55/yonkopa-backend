2d4eb5748612b23477b7aae1ff83bd65c80fc8e9
Joseph Frimpong <frimpongj190@gmail.com>
2026-03-03 20:44:22 +0000

    Initial commit

--- server.js
+++ server.js
@@ -28,8 +28,11 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  
 });
+
+//const db = mysql.createConnection(process.env.MYSQL_URL);
 
 db.connect(err => {
   if (err) console.error('❌ Database connection failed:', err);
